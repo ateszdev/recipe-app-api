d297a72e83672972f0afaaaba66e27b157e288b4
atesz <ateszpost@yahoo.com>
2023-03-08 00:31:48 +0100

    Test

---
+++ app/app/calc.py
@@ -0,0 +1,18 @@
+"""
+
+    Calculator functions
+
+"""
+
+
+def add(x, y):
+    
+    """ Add x and y and return result """
+    
+    return x + y
+
+def subtract(x, y):
+    
+    """ Substract x from y and return result """
+    
+    return y - x
